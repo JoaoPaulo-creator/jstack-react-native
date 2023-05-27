23ec791fdfa4ee081fa110e4fd82c038bd4b9a2c
JoaoPaulo-creator <joaosarto@gmail.com>
2023-05-26 23:12:08 -0300

    First commit"

---
+++ App.js
@@ -0,0 +1,7 @@
+/* eslint-disable react-native/no-inline-styles */
+import { Text } from 'react-native';
+function App() {
+  return <Text style={{ marginTop: 10, color: 'red' }}>Hello World!</Text>;
+}
+
+export default App;
