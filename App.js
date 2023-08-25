a18e75f19a883f5f37bb91725ff45297b712a979
JoaoPaulo-creator <joaosarto@gmail.com>
2023-08-24 22:17:15 -0300

    props de tamanho de font, selectionColor, entre outras

--- App.js
+++ App.js
@@ -1,7 +1,24 @@
 /* eslint-disable react-native/no-inline-styles */
-import { Text } from 'react-native';
+import {View, Text } from 'react-native';
 function App() {
-  return <Text style={{ marginTop: 10, color: 'red' }}>Hello World!</Text>;
+  return (
+    <View>
+      <Text
+      style={{ 
+        marginTop: 300, 
+        fontSize: 30, 
+      }}
+      selectable // essa propriedade permite que você selecione o texto, por default, nenhum texto eh selecionavel, a menos que tenha essa prop
+      selectionColor={'#FF0000'} // essa prop funciona apenas no android
+      numberOfLines={1}
+      ellipsizeMode='clip'
+      allowFontScaling={true}
+      maxFontSizeMultiplier={1.5}
+      >
+        Isso texto de exemplo muito show mesmo, cara. Daora demais mesmo
+      </Text>
+    </View>
+  )
 }
 
 export default App;
